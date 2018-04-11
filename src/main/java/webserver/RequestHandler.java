7ee17a988fa5850eca7dbb213edc93846d2ef81a
jb7959 <jb7959@naver.com>
2018-04-11 17:58:08 +0900

    get 가입까지 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,7 +3,10 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.IOUtils;
@@ -18,23 +21,37 @@ public class RequestHandler extends Thread {
     }
 
     public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             DataOutputStream dos = new DataOutputStream(out);
-            //HTTP BODY의 값은 byte[]로 표현되어야 한다.
+
+            ///////////////////////HTTP REQUEST 해석//////////////////////////////////////
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            String  req = IOUtils.readData(reader,in.read());
+            String  req = IOUtils.readData(reader,connection.getReceiveBufferSize());
             String [] headAndBody = req.split("\n\n");
             String head = headAndBody[0];
+            log.debug("{}",head);
             head.split("\\s+"); //띄어쓰기를 기준으로 분류
             String uri = head.split("\\s+")[1];
             uri= defaultPath+uri;
+            ///////////////////////HTTP REQUEST 해석//////////////////////////////////////
+
+            if(uri.split("\\?")[0].equals(defaultPath+"/user/create")){
+               String param = uri.split("\\?")[1];
+
+               Map<String,String> map = new HashMap<>();
+
+               for(String p:param.split("&")){
+                   String [] temp = p.split("=");
+                    map.put(temp[0],temp[1]);
+               }
+               User user = new User(map.get("userId"),map.get("password"),map.get("name"),map.get("email"));
+               log.info("추가된 유저 {}",user.toString());
+                uri = defaultPath+"/user/list.html";
+            }
             byte[] body = Files.readAllBytes(new File(uri).toPath()); //NIO를 활용한 File to byte[]body
-            FileReader
             response200Header(dos, body.length);
             responseBody(dos, body);
+
         } catch (IOException e) {
             log.error(e.getMessage());
         }
