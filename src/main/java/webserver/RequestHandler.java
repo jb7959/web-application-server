634bddc72ffefabb3018d8b69eaadbf02e758eca
jb7959 <jb7959@naver.com>
2018-04-16 15:24:35 +0900

    POST 가입까지 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -27,27 +27,38 @@ public class RequestHandler extends Thread {
             ///////////////////////HTTP REQUEST 해석//////////////////////////////////////
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
             String  req = IOUtils.readData(reader,connection.getReceiveBufferSize());
-            String [] headAndBody = req.split("\n\n");
-            String head = headAndBody[0];
-            log.debug("{}",head);
-            head.split("\\s+"); //띄어쓰기를 기준으로 분류
-            String uri = head.split("\\s+")[1];
+            String [] headAndBody = req.split("\\r\\n\\r\\n"); //줄바꿈을 기준으로 분리(한줄공백)
+            String httpHead = headAndBody[0];
+            String httpBody ="";
+            log.debug("HttpHead:::::::::::::::::::::::::{}",req);
+            httpHead.split("\\s+"); //띄어쓰기를 기준으로 분류
+
+            //todo Http Model 만들어서 해결 할 것 18.04.16
+            String method = httpHead.split("\\s+")[0];
+            String uri = httpHead.split("\\s+")[1];
+            String httpVersion = httpHead.split("\\s+")[2];
             uri= defaultPath+uri;
-            ///////////////////////HTTP REQUEST 해석//////////////////////////////////////
 
-            if(uri.split("\\?")[0].equals(defaultPath+"/user/create")){
-               String param = uri.split("\\?")[1];
-
-               Map<String,String> map = new HashMap<>();
-
-               for(String p:param.split("&")){
-                   String [] temp = p.split("=");
-                    map.put(temp[0],temp[1]);
-               }
-               User user = new User(map.get("userId"),map.get("password"),map.get("name"),map.get("email"));
-               log.info("추가된 유저 {}",user.toString());
-                uri = defaultPath+"/user/list.html";
+            if(method.equals("POST")){
+                httpBody = headAndBody[1];
             }
+
+            ///////////////////////HTTP REQUEST 해석//////////////////////////////////////
+            /*회원가입요청 /user/create */
+            if(uri.split("\\?")[0].equals(defaultPath+"/user/create")){
+                Map<String,String> map = null;
+                //get일 경우
+                if(method.equals("GET")){
+                    map = getParam(uri);
+                }else if(method.equals("POST")){
+                    map = getParam(httpBody);
+                }
+                User user = new User(map.get("userId"),map.get("password"),map.get("name"),map.get("email"));
+                log.info("추가된 유저 {}",user.toString());
+                uri = defaultPath+"/user/list.html"; //완료후 LIST 페이지로
+            }
+            /*회원가입요청끝*/
+
             byte[] body = Files.readAllBytes(new File(uri).toPath()); //NIO를 활용한 File to byte[]body
             response200Header(dos, body.length);
             responseBody(dos, body);
@@ -55,6 +66,21 @@ public class RequestHandler extends Thread {
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    private Map getParam(String paramString){
+        String param = paramString;
+        if(paramString.contains("?")){
+            param = paramString.split("\\?")[1];
+        }
+
+        Map<String,String> map = new HashMap<>();
+
+        for(String p:param.split("&")){
+            String [] temp = p.split("=");
+            map.put(temp[0],temp[1]);
+        }
+        return map;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
